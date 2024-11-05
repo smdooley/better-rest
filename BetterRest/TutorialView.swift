b2e4586cbf963ad1b0ab7a07a7857f9916e74cec
Sean Dooley <s.dooley@ctidigital.com>
2024-11-05 15:27:03 +0000

    Building a basic layout

--- BetterRest/TutorialView.swift
+++ BetterRest/TutorialView.swift
@@ -8,9 +8,36 @@
 import SwiftUI
 
 struct TutorialView: View {
+    @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date.now
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+            
+            DatePicker("Please enter a date and time", selection: $wakeUp)
+            
+            DatePicker("", selection: $wakeUp)
+            
+            DatePicker("Please enter a date and time", selection: $wakeUp)
+                .labelsHidden()
+            
+            DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .date)
+            
+            DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+            
+            DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
+        }
+        .padding()
     }
+    
+    /*func exampleDates() {
+        // create a second Date instance set to one day in seconds from now
+        let tomorrow = Date.now.addingTimeInterval(86400)
+
+        // create a range from those two
+        let range = Date.now...tomorrow
+    }*/
 }
 
 #Preview {
