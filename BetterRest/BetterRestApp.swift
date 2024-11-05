b23b4cf894858988a8a789fa0d7f77534b74b8bf
Sean Dooley <s.dooley@ctidigital.com>
2024-11-05 14:51:04 +0000

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Sean Dooley on 05/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
